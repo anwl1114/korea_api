2ab9386c83c8c5aaed13b7ce8b7d1d9f207b8c2e
anwl1114 <toffl1114@naver.com>
2022-03-11 18:02:09 +0900

    InCompletion

--- app/src/main/java/com/example/api_korea/repository/KoreaRepository.kt
+++ app/src/main/java/com/example/api_korea/repository/KoreaRepository.kt
@@ -1,8 +1,7 @@
 package com.example.api_korea.repository
 
-import android.content.ClipData
 import com.example.api_korea.KoreaClient
-import com.example.api_korea.MainActivity
+import com.example.api_korea.ui.MainActivity
 import com.example.api_korea.data.Item
 import retrofit2.Call
 import retrofit2.Callback
