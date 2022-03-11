2d1543bd3e4948e5f93ff8930c21f589c61c123b
anwl1114 <toffl1114@naver.com>
2022-03-11 19:18:09 +0900

    InCompletion..ㅠㅠㅠ

--- app/src/main/java/com/example/api_korea/ui/MainActivity.kt
+++ app/src/main/java/com/example/api_korea/ui/MainActivity.kt
@@ -2,23 +2,28 @@ package com.example.api_korea.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import android.widget.Toast
-import com.example.api_korea.R
 import com.example.api_korea.databinding.ActivityMainBinding
 import com.example.api_korea.repository.KoreaRepository
 import com.google.android.gms.drive.events.CompletionListener
 
-class MainActivity : AppCompatActivity(), CompletionListener  {
-
+class MainActivity() : AppCompatActivity(), CompletionListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var notionAdapter: NotionAdapter
-    private val koreaRepository : KoreaRepository()
+    private val koreaRepository = KoreaRepository()
     private var page = 1
+
+    constructor(parcel: Parcel) : this() {
+        page = parcel.readInt()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
     override fun loadComplete(data: Data) {
@@ -34,6 +39,24 @@ class MainActivity : AppCompatActivity(), CompletionListener  {
    override fun responseIsNotSuccesful(code :Int) {
         Toast.makeText(this, "사이트가 응답하지 않습니다", Toast.LENGTH_SHORT).show()
         Log.v("log", code.toString())
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(page)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
     }
 
 
